973b5cb654bf68e631a3000874af81187f186652
gerald <geraldacunasoftware@gmail.com>
2024-11-07 22:03:19 -0600

    Allowed hosts[*]

--- mi_api/settings.py
+++ mi_api/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tg+17^c96&8cwd8&c1@eb%hucal67(i0=wd72sv*k9wm5ho7n!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
